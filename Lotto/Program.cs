9211319fd84ed736f9c01d9b4dce154a17eeb50d
Marci02 <nagymarci.0218@gmail.com>
2023-02-02 13:13:01 +0100

    3. feladat van kész

--- Lotto/Program.cs
+++ Lotto/Program.cs
@@ -11,17 +11,26 @@ namespace Lotto
     {
 
         static int[] het52 = new int[5];
+        static int het;
         static void Main(string[] args)
         {
 
             ElsoFeladat();
+            Console.WriteLine();
             MasodikFeladat();
+            Console.WriteLine();
             HarmadikFeladat();
+            Console.WriteLine();
             NegyedikFeladat();
+            Console.WriteLine();
             OtodikFeladat();
+            Console.WriteLine();
             HatodikFeladat();
+            Console.WriteLine();
             HetedikFeladat();
+            Console.WriteLine();
             NyolcadikFeladat();
+            Console.WriteLine();
             KilencedikFeladat();
 
 
@@ -57,21 +66,68 @@ namespace Lotto
         private static void NegyedikFeladat()
         {
             Console.WriteLine("4. feladat");
+            AdatokBeolvasasa();
+
+        }
+
+        private static void AdatokBeolvasasa()
+        {
+            StreamReader be = new StreamReader("lottosz.dat");
+
+            while (!be.EndOfStream)
+            {
+                string[] a = be.ReadLine().Split(' ');
+            }
+
+            be.Close();
         }
 
         private static void HarmadikFeladat()
         {
             Console.WriteLine("3. feladat");
+            Console.Write("Adjon meg egy 1-51 közötti számot: ");
+            het = Convert.ToInt32(Console.ReadLine())-1;
         }
 
         private static void MasodikFeladat()
         {
             Console.WriteLine("2. feladat");
+
+            for (int i = 0; i < het52.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < het52.Length; j++)
+                {
+                    if (het52[min] > het52[j])
+                    {
+                        min = j;
+                    }
+                }
+                int temp = het52[i];
+                het52[i] = het52[min];
+                het52[min] = temp;
+            }
+
+            Console.Write("A lottó számok növekvő sorrendben: ");
+            foreach (var item in het52)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+
         }
 
         private static void ElsoFeladat()
         {
             Console.WriteLine("1. feladat");
+
+
+            for (int i = 0; i < het52.Length; i++)
+            {
+                Console.Write($"Kérem a(z) {i+1}. számot: ");
+                het52[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
         }
     }
 }
